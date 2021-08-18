171f90854110965ba35e929433fc8e0673e2b30a
SomuTech <somuyadav018@gmail.com>
2021-08-18 10:18:47 +0530

    functions

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
+//Temperature conversion
 function toFarenheit(celsius) 
 {
     console.log("celsius: "+celsius);
     var Fahrenheit = (celsius*9/5+32);
     //console.log("Fahrenheit:" + Fahrenheit);
     return Fahrenheit;
+}
+//Area of Rectangle
+function areaOfRectangle(length,width)
+{
+    var area = length*width;
+    //console.log("Area of Rectangle is: "+ area);
+    return area;
 }
