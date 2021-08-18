2a93d61422f95b8e0893b69ebd5a9d1168db42a7
SomuTech <somuyadav018@gmail.com>
2021-08-18 10:06:52 +0530

    functions

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ function toFarenheit(celsius)
 {
     console.log("celsius: "+celsius);
     var Fahrenheit = (celsius*9/5+32);
-    console.log("Fahrenheit:" + Fahrenheit);
+    //console.log("Fahrenheit:" + Fahrenheit);
+    return Fahrenheit;
 }
